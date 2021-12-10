be88147046517f90cca6904dbae61d5f8e7c2c21
Stephanie Jones <stephanie.nicole.jones78@gmail.com>
2021-12-10 09:57:13 -0600

    final acquire module

--- acquire.py
+++ acquire.py
@@ -1,13 +1,19 @@
-# FUNCTION TO ACQUIRE RAW DATA 
-'''
-Use this module to acquire project data
-    - Global variables:
-        - sql_connect_str
+#  ACQUIRE              #######################################################################
+#Use this module to acquire project data
+
+## CONTENTS             #######################################################################
+''' 
+    I. IMPORTS
+    
+    II. GLOBAL VARIABLES
+        - db
         - url
         - query
-        - db
-    - Acquire function:
-        - acquire_data(filename)
+        - file_name
+        
+    III. acquire_data() function
+    
+    IV. VERIFY COUNT
 '''
 
 
@@ -21,16 +27,13 @@ import os
 
 ## GLOBAL VARIABLES     #######################################################################
 
-#database to use to pull the data
-db = zillow
+#database to use to pull the data from SQL server
+db = 'zillow'
 
-#string with sql db connection credentials
-sql_connect_str = f'mysql+pymysql://{user}:{password}@{host}/db'
+#url string with connection credentials used to access Zillow database from SQL server 
+url = f'mysql+pymysql://{user}:{password}@{host}/{db}'
 
-#url string to access Zillow database from SQL server
-url = f'mysql+pymysql://{user}:{password}@{host}/zillow'
-
-#sql query to pull data
+#SQL query that returns selected data from server
 query = '''
 SELECT bathroomcnt,
         bedroomcnt,
@@ -40,19 +43,24 @@ SELECT bathroomcnt,
         yearbuilt,
         taxvaluedollarcnt
 FROM properties_2017
-WHERE propertylandusetypeid = 261
+JOIN predictions_2017 USING(parcelid)
+WHERE propertylandusetypeid = 261;
 '''
+
+#name of the .csv file with project data
+file_name = 'zillow.csv'
+
 
 ## ACQUIRE FUNCTION     #######################################################################
 
-def acquire_data(filename):
+def acquire_data():
     '''
-    This function takes in as an argument the .csv file with the raw project data and, if the file exists
+    This function checks for the .csv file with the raw project data and, if the file exists
     locally, writes its contents to a pandas df.
     
     If the raw project data file does not exist locally, the query and url variables, which are defined 
-    globally in the acquire.py file, are used to access the SQL db with the db credentials and SQL query 
-    of the desired data.
+    globally in the acquire.py file, are used to access the SQL and the queried data is written to a 
+    pandas df.
     
     This function returns a df of the raw queried data. 
     '''
@@ -64,3 +72,11 @@ def acquire_data(filename):
     # if project data csv file does not exist locally, connect to SQL db and save query as df
     else:
         return pd.read_sql(query, url)
+
+## VERIFY COUNT         #######################################################################
+
+#row count of data acquired, will be used for notebook in a print statement to verify the number of observations
+verify_count = f'After acquiring our data, we have {acquire_data().shape[0]} observations, ready to be prepapred.'
+
+
+
